e6ee801368da9d99778f4391c212b4e8aabc7f11
radioation <greggallardo@gmail.com>
2025-09-13 20:37:13 -0500

    working on game state tests

--- chess/server/rlsf_chess/test_game.py
+++ chess/server/rlsf_chess/test_game.py
@@ -8,8 +8,16 @@ def test_new_game_init():
     g = ChessGame('S', 'W')
     assert isinstance( g.id, str )
     assert g.id != ""
+    assert g.id.isalnum() is True
     assert isinstance( g.player_1_id, str )
+    assert g.player_1_id != ""
+    assert g.player_1_id.isalnum() is True
     assert isinstance( g.player_2_id, str )
+    assert g.player_2_id != ""
+    assert g.player_2_id.isalnum() is True
+
+    assert g.player_1_id != g.player_2_id
+
     assert g.mode == 'S'
     assert g.player_1_side == 'W'
     assert g.curr_player == 1
@@ -17,3 +25,10 @@ def test_new_game_init():
     assert g.board.is_valid() is True
     assert g.board.is_game_over() is False
     assert g.moves == []
+
+def test_new_game_double_black_init():
+    g = ChessGame('D', 'B')
+    assert g.mode == 'D'
+    assert g.player_1_side == 'B'
+
+
